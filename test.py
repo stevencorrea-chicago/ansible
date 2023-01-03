78cf46357ebf0251d8e86c614d8db0b7a18d8180
stevencorrea-chicago <40609698+stevencorrea-chicago@users.noreply.github.com>
2023-01-02 18:18:04 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
+import time
+
 print("This is a test python application located on GitHub")
 print("This file has changed")
 
-for i in range(100):
+for i in range(10):
   print (i)
+  
+print (time.strftime("%m/%d/%Y, %H:%M:%S", time.localtime()))
